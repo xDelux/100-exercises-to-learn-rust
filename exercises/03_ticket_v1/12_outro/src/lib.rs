2e97ba51265bed6406b3280ec00a26875c1c3635
Stefan Luxhøj <stefan@onewayesport.dk>
2025-02-05 11:16:15 +0100

    exercise 3

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,80 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+
+pub mod outro {
+    pub struct Order {
+        product_name: String,
+        quantity: u32,
+        unit_price: u32
+    }
+
+    impl Order {
+
+        pub fn new(product_name: String, quantity: u32, unit_price: u32) -> Order {
+            if product_name.len() > 300 {
+                panic!("Product name too long!");
+            }
+
+            if product_name.is_empty() {
+                panic!("Product name empty!");
+            }
+
+            if quantity <= 0 {
+                panic!("Quantity must not be zero!");
+            }
+
+            if unit_price <= 0 {
+                panic!("Unit price must not be zero!");
+            }
+            
+            return Order {
+                product_name,
+                quantity,
+                unit_price
+            };
+        }
+
+        pub fn product_name(&self) -> &String {
+            &self.product_name
+        }
+    
+        pub fn quantity(&self) -> &u32 {
+            &self.quantity
+        }
+    
+        pub fn unit_price(&self) -> &u32 {
+            &self.unit_price
+        }
+
+        pub fn set_product_name(&mut self, new_product_name: String) {
+            if new_product_name.is_empty() {
+                panic!("Product name empty!");
+            }
+            if new_product_name.len() > 300 {
+                panic!("Product name too long!");
+            }
+
+
+            self.product_name = new_product_name;
+        }
+        pub fn set_quantity(&mut self, new_quantity: u32) {
+            if new_quantity <= 0 {
+                panic!("Quantity must not be zero!");
+            }
+            self.quantity = new_quantity;
+        }
+
+        pub fn set_unit_price(&mut self, new_unit_price: u32) {
+            if new_unit_price <= 0 {
+                panic!("Unit price must not be zero!");
+            }
+            self.unit_price = new_unit_price;
+        }
+
+        pub fn total(&self) -> u32 {
+            self.quantity.saturating_mul(self.unit_price)
+        }
+
+    }
+}
--- exercises/03_ticket_v1/12_outro/tests/integration.rs
+++ exercises/03_ticket_v1/12_outro/tests/integration.rs
@@ -1,4 +1,4 @@
-use outro_02::Order;
+use outro_02::outro::Order;
 
 // Files inside the `tests` directory are only compiled when you run tests.
 // As a consequence, we don't need the `#[cfg(test)]` attribute for conditional compilation—it's
